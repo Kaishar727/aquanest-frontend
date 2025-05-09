94c599f9b98010987080489133836c35943e92b4
jamesdry <jamesdrykwan@gmail.com>
2025-05-09 12:06:25 +0700

    resized notification maincard

--- src/pages/dashboard/default.jsx
+++ src/pages/dashboard/default.jsx
@@ -495,13 +495,14 @@ export default function DashboardDefault() {
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3} sx={{ height: '100vh' }}>
           <Grid container direction="column" spacing={2} sx={{ height: '100%' }}>
-            <Grid item sx={{ height: '100%' }}>
+            <Grid item sx={{ height: '100%', width: { xs: '100%', sm: 'auto' }}}>
               <MainCard
                 sx={{
-                  height: '100%',
+                  height: '102%',
                   display: 'flex',
                   flexDirection: 'column',
-                  p: 0
+                  p: 0,
+                  
                 }}
               >
                 <Typography
